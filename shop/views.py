f48ecb3ac39c48ac672bd02c62ef672a2cbebaf6
darshan@2297 <darshanvankawala2297@gmail.com>
2020-08-30 02:17:38 +0530

    initial commit

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 class homepageview(View):
     def get(self,request,*args,**kwargs):
         products = product.objects.all()
-        return render(request ,"home.html",{"products":products})
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(products,40)
+        try:
+            product_list = paginator.page(page)
+        except PageNotAnInteger:
+            product_list = paginator.page(1)
+        except EmptyPage:
+            product_list = paginator.page(paginator.num_pages)
+        return render(request ,"home.html",{"product_list":product_list})
         
 class product_view(View):
     def get(self,request,pk,*args,**kwargs):
